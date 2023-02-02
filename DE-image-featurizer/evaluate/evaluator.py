c6e0178ae92342c0b23c501fd9712422d035c1a6
rahulthakurakridata <rahul.thakur@akridata.com>
2023-02-02 10:09:08 +0530

    1. The benefits of classes like (instance attributes and static-methods) has been added to the evaluate module.

--- DE-image-featurizer/evaluate/evaluator.py
+++ DE-image-featurizer/evaluate/evaluator.py
@@ -1,4 +1,11 @@
-"""Provides the functinality to compare different image featurizers"""
+"""
+evaluate.py
+Provides the functinality to compare different image featurizers
+Creates a featurizer from tf-hub or traditional repositoy based models.
+Applies PCA to reduce feature dimensions.
+Provides KNN accuracy scores for different set of dimensions
+"""
+
 import tensorflow as tf
 import numpy as np
 from tqdm import tqdm
@@ -8,6 +15,8 @@ from sklearn.decomposition import PCA
 import pandas as pd
 import tensorflow_hub as hub
 from ..featurize.descriptor import HOG, LBP, ORB, SIFT
+
+
 class ImageFeatureEvaluator:
     """
     Provides the functionality to compare different image featurizers using KNN accuracy scores.
@@ -15,18 +24,21 @@ class ImageFeatureEvaluator:
     2. Features are computed using the featurizer provided.
     3. Comparative analysis is shown on different numbers of reduced dimensions using PCA.
 
-    Parameters
+    Attributes
     ----------
 
-    data_dir : string
-        Provides the path to parent directory where data is present
-    featurizer_selected : string
-        Hyperlink to the Image featurizer module
     batch_size: integer
         Size of batches of data.
     structure: boolean
         Whether the directory is further divided into train and test folders.
-
+    train_ds :  A `tf.data.Dataset` object
+            The training dataset
+    test_ds :  A `tf.data.Dataset` object
+            The testing dataset
+    image_featurizer : function
+        The image featurizer model to be used for extracting features.
+    preprocessor : function
+        The basic preprocessor function required to standardise input
 
     Examples
     ----------
@@ -67,178 +79,103 @@ class ImageFeatureEvaluator:
     >>> dataset_url = "https://storage.googleapis.com/" \
     "download.tensorflow.org/example_images/flower_photos.tgz"
     >>> archive = tf.keras.utils.get_file(origin=dataset_url, extract=True)
-    ...
     >>> data_dir = pathlib.Path(archive).with_suffix('')
     >>> fan = ImageFeatureEvaluator(data_dir,\
     'https://tfhub.dev/google/imagenet/mobilenet_v1_025_224/feature_vector/5',\
     32,False)
-
-    >>> train_ds, _ = fan.load_data()
-    Found ...
-    >>> featurizer, preprocessor = fan.load_featurizer()
-    >>> train_features, train_labels = fan.extract_features(featurizer,\
-    preprocessor,train_ds,True)
+    Found...
+    >>> train_features, train_labels = fan._extract_features(True)
     >>> train_features.shape
     (2936, 256)
     >>> fan = ImageFeatureEvaluator(data_dir,'hog',32,False)
-    >>> featurizer, preprocessor = fan.load_featurizer()
-    >>> train_features, train_labels = fan.extract_features(featurizer,\
-    preprocessor,train_ds,True)
+    Found...
+    >>> train_features, train_labels = fan._extract_features(True)
     >>> train_features.shape
     (2936, 1568)
     """
 
-    def __init__(self, data_dir, featurizer_selected, batch_size, structure):
-        self.data_path = data_dir
-        self.featurizer_selected = featurizer_selected
+    def __init__(self, data_dir, featurizer_selected, batch_size=32, structure=False):
+        """
+        Constructor for the ImageFeatureEvaluator class
+
+        Parameters
+        -------------
+        data_dir : string
+            The path to the image dataset
+        featurizer_selected : string
+            The image featurizer to be used
+        batch_size : int, default=32
+            The size of the batch of the data
+        structure: boolean , default=False
+            Whether the directory is further divided into train and test folders.
+        """
+
         self.batch_size = batch_size
         self.structure = structure
+        self._load_data(data_dir)
+        self._load_featurizer(featurizer_selected)
 
-    def load_data(self):
+    def _load_data(self,data_dir):
         """
         Creates training and testing dataset from a directory
         of images where labels are inferred from directory structure
 
-        Returns
-        -------
-        train_ds :  A `tf.data.Dataset` object
-            The training dataset
-
-        test_ds :  A `tf.data.Dataset` object
-            The testing dataset
+        Parameters
+        -------------
+        data_dir : string
+            The path to the image dataset
         """
+
         if self.structure:
-            train_ds = tf.keras.utils.image_dataset_from_directory(self.data_path + '/train',
+            self.train_ds = tf.keras.utils.image_dataset_from_directory(data_dir + '/train',
             seed=123, batch_size=self.batch_size)
-            val_ds = tf.keras.utils.image_dataset_from_directory(self.data_path + '/test',
+            self.val_ds = tf.keras.utils.image_dataset_from_directory(data_dir + '/test',
             seed=123, batch_size=self.batch_size)
         else:
-            train_ds = tf.keras.utils.image_dataset_from_directory(self.data_path,
+            self.train_ds = tf.keras.utils.image_dataset_from_directory(data_dir,
             validation_split=0.2,subset='training',seed=123, batch_size=self.batch_size)
-            val_ds = tf.keras.utils.image_dataset_from_directory(self.data_path,
+            self.val_ds = tf.keras.utils.image_dataset_from_directory(data_dir,
             validation_split=0.2,subset='validation',seed=123, batch_size=self.batch_size)
-        return train_ds, val_ds
 
-    def evaluate(self):
-        """
-          Consolidates all the below defined steps of the process of image featurizer evaluation:
-          1. Loads the train and test set
-          2. Loads the featurizer object
-          3. Calculates the features for the train and test set
-          4. Computes the results dataframe containing the accuracy scores.
-
-        Returns
-        -------
-        results_frame : A pandas.DataFrame object
-            Pandas dataframe which consists of the evaluation statistics.
-
-        """
-
-        train_ds, val_ds = self.load_data()
-        featurizer, preprocessor = self.load_featurizer()
-        train_features, train_labels = self.extract_features(featurizer,
-                                        preprocessor, train_ds, True)
-        test_features, test_labels = self.extract_features(featurizer,
-                                        preprocessor, val_ds, False)
-        results_frame = self.accuracy_stats(train_features,
-                            test_features, train_labels, test_labels)
-        return results_frame
-
-    def feature_loader_and_provide_score(self, train_features_path, train_labels_path,
-                                         test_features_path, test_labels_path):
-        """
-        Computes the stats related to evaluation metric by using pre computed features
-
-
-        Parameters
-        ----------
-
-        train_features_path : string
-            Provides the path to the precomputed features for training data.
-
-        train_labels_path : string
-            Provides the path to the labels corresponding to training data.
-
-        test_features_path : string
-            Provides the path to the precomputed features for the testing data.
-
-        test_labels_path : string
-            Provides the path to the labels corresponding to testing data.
-
-        Returns
-        -------
-
-        results_frame : A pandas.DataFrame object
-            Pandas dataframe which consists of the evaluation statistics.
-
-        """
-
-        train_features = np.load(train_features_path)
-        train_labels = np.load(train_labels_path)
-        test_features = np.load(test_features_path)
-        test_labels = np.load(test_labels_path)
-        results_frame = self.accuracy_stats(train_features,
-                            test_features, train_labels, test_labels)
-
-        return results_frame
-
-    def load_featurizer(self):
+    def _load_featurizer(self,featurizer_selected):
         """
         Instantiates the model from TF-hub library or
         from the featurizers present in the DE-image-featurizer repository
         and also creates a preprocessor to standardise the input.
 
-        Returns
-        -------
-
-        image_featurizer : function
-            The image featurizer model to be used for extracting features.
-
-        preprocessor : function
-            The basic preprocessor function required to standardise input
+        Parameters
+        -------------
+        featurizer_selected : string
+            The image featurizer to be used
         """
 
-        if self.featurizer_selected.lower() == 'hog':
+        if featurizer_selected.lower() == 'hog':
             hog = HOG()
-            image_featurizer = hog.fit_transform
-        elif self.featurizer_selected.lower() == 'lbp':
+            self.image_featurizer = hog.fit_transform
+        elif featurizer_selected.lower() == 'lbp':
             lbp = LBP()
-            image_featurizer = lbp.fit_transform
-        elif self.featurizer_selected.lower() == 'orb':
+            self.image_featurizer = lbp.fit_transform
+        elif featurizer_selected.lower() == 'orb':
             orb = ORB()
-            image_featurizer = orb.fit_transform
-        elif self.featurizer_selected.lower() == 'sift':
+            self.image_featurizer = orb.fit_transform
+        elif featurizer_selected.lower() == 'sift':
             sift = SIFT()
-            image_featurizer = sift.fit_transform
+            self.image_featurizer = sift.fit_transform
 
         else:
-            image_featurizer = hub.KerasLayer(self.featurizer_selected,
+            self.image_featurizer = hub.KerasLayer(featurizer_selected,
                                               input_shape=(224, 224, 3), trainable=False)
-        preprocessor = tf.keras.Sequential([tf.keras.layers.Resizing(224, 224),
+        self.preprocessor = tf.keras.Sequential([tf.keras.layers.Resizing(224, 224),
                                             tf.keras.layers.Rescaling(1. / 255)])
 
-        return image_featurizer, preprocessor
-
-    def extract_features(self, image_featurizer, preprocessor, input_data, train):
+    def _extract_features(self,is_train):
         """
         Computes the features for the input data using the image featurizer
 
         Parameters
         ----------
 
-        image_featurizer : function
-            The image featurizer model imported from TF-hub
-
-        preprocessor : function
-        The preprocessing  function comprising of rescaling and resizing.
-
-        input_data : A `tf.data.Dataset` object.
-            The data containing images as elements
-
-        saving_path : string
-            The path to save extracted features
-
-        train : boolean
+        is_train : boolean
             To distinguish between training and testing set
 
         Returns
@@ -254,10 +191,10 @@ class ImageFeatureEvaluator:
 
         features_array = None
         input_labels = None
-
+        input_data = self.train_ds if is_train else self.val_ds
         for image_batch, labels_batch in tqdm(input_data):
-            preprocessed_image_batch = preprocessor(image_batch)
-            image_batch_features = image_featurizer(preprocessed_image_batch)
+            preprocessed_image_batch = self.preprocessor(image_batch)
+            image_batch_features = self.image_featurizer(preprocessed_image_batch)
             image_batch_features = image_batch_features if \
             isinstance(image_batch_features,list) else image_batch_features
             if features_array is None:
@@ -268,7 +205,11 @@ class ImageFeatureEvaluator:
                 input_labels = labels_batch
             else:
                 input_labels = np.concatenate([input_labels, labels_batch.numpy()])
-        if train:
+
+        if features_array is None:
+            raise TypeError
+
+        if is_train:
             np.save('train_features.npy', features_array)
             np.save('train_labels.npy', input_labels)
         else:
@@ -282,8 +223,8 @@ class ImageFeatureEvaluator:
                 [:(-features_array.ndim + 1)], -1)
         return features_array, input_labels
 
-    def accuracy_scores(self, train_features, train_labels,
-                        test_features, test_labels):
+    @staticmethod
+    def _accuracy_scores(train_features,train_labels,test_features,test_labels):
         """
         Applies KNeighborsClassifier on the input data
         and return the accuracy obtained on training and testing sets.
@@ -328,8 +269,8 @@ class ImageFeatureEvaluator:
 
         return train_accuracy, test_accuracy
 
-    def accuracy_stats(self, train_features, test_features,
-                       train_labels, test_labels):
+    @staticmethod
+    def _accuracy_stats(train_features,train_labels,test_features,test_labels):
         """
         Applies PCA on the data over several number of
         dimensions and return a consolidated evaluation statistics.
@@ -366,14 +307,64 @@ class ImageFeatureEvaluator:
             pca = PCA(n_components=dimension)
             reduced_train_features = pca.fit_transform(train_features)
             reduced_test_features = pca.transform(test_features)
-            train_accuracy, test_accuracy = self.accuracy_scores(reduced_train_features,
-            train_labels,
-            reduced_test_features, test_labels)
+            train_accuracy, test_accuracy = ImageFeatureEvaluator._accuracy_scores\
+            (reduced_train_features,train_labels,reduced_test_features,test_labels)
             train_accuracies.append(train_accuracy)
             test_accuracies.append(test_accuracy)
         results_frame = pd.DataFrame(list(zip(dimensions, train_accuracies, test_accuracies)),
                                      columns=['Dimensions', 'Train_Accuracy', 'Test_Accuracy'])
 
+        return results_frame
+
+    @staticmethod
+    def feature_loader_and_provide_score(main_path):
+        """
+        Computes the stats related to evaluation metric by using pre-computed features
+
+
+        Parameters
+        ----------
+
+        main_path : string
+            Provides the path to the directory that contains precomputed features and labels.
+            The directory should contains numpy files with names 'train_features','train_labels',
+            'test_features','test_labels'.
+
+        Returns
+        -------
+
+        results_frame : A pandas.DataFrame object
+            Pandas dataframe which consists of the evaluation statistics.
+
+        """
+
+        train_features = np.load(main_path+'/train_features.npy')
+        train_labels = np.load(main_path+'/train_labels.npy')
+        test_features = np.load(main_path+'/test_features.npy')
+        test_labels = np.load(main_path+'/test_labels.npy')
+        results_frame = ImageFeatureEvaluator._accuracy_stats(train_features,train_labels,
+                        test_features,test_labels)
+
+        return results_frame
+
+
+    def evaluate(self):
+        """
+          Consolidates all the below defined steps of the process of image featurizer evaluation:
+          1. Calculates the features for the train and test set
+          2. Computes the results dataframe containing the accuracy scores.
+
+        Returns
+        -------
+        results_frame : A pandas.DataFrame object
+            Pandas dataframe which consists of the evaluation statistics.
+
+        """
+
+        train_features,train_labels = self._extract_features(True)
+        test_features, test_labels = self._extract_features(False)
+        results_frame = ImageFeatureEvaluator._accuracy_stats(train_features,
+                                            train_labels,test_features, test_labels)
         return results_frame
 
 
